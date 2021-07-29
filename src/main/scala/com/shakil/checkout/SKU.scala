83b70b50d33e7b1efeb0c99064e6c356f0e1a4b3
ShakilH96 <Shakilhaque96@gmail.com>
2021-07-29 22:23:39 +0100

    ferferf

--- src/main/scala/com/shakil/checkout/SKU.scala
+++
@@ -1,8 +0,0 @@
-package com.shakil.checkout
-
-sealed trait SKU {val unitPrice: Double}
-
-case object A extends SKU { val unitPrice = 0.50 }
-case object B extends SKU { val unitPrice = 0.30 }
-case object C extends SKU { val unitPrice = 0.20 }
-case object D extends SKU { val unitPrice = 0.15 }
